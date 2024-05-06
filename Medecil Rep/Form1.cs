25907d600b7b273abc88f0ce39aa710af4f4d917
bm941 <bm941@LAPTOP-47G4M9MA>
2024-05-06 15:37:15 +0300

    Add project files.

---
+++ Medecil Rep/Form1.cs
@@ -0,0 +1,35 @@
+namespace Medecil_Rep
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
